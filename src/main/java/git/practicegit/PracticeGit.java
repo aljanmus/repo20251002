04436515cdcdd6f6e4267f0bdcf9716675317146
Mustafa aljanabi <aljanmus@sheridancollege.ca>
2025-09-25 08:31:04 -0400

    revision 1

---
+++ src/main/java/git/practicegit/PracticeGit.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package git.practicegit;
+
+/**
+ *
+ * @author musta
+ */
+public class PracticeGit {
+
+    public static void main(String[] args) {
+        System.out.println("Code version 1!");
+    }
+}
